14ab6a5db89dcc43e402c58e2a91854ae448d0af
tinyface187 <n.forester.magic@gmail.com>
2022-09-19 15:02:59 -0700

    clipbard v1.2.3

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,22 +1,39 @@
 import React from "react";
-import { AiOutlineMail, AiOutlinePhone } from "react-icons/ai";
+import { AiOutlineMail, AiOutlinePhone, AiFillCloseCircle } from "react-icons/ai";
 
-import { Title, Line, Content, Container, Item } from "./FooterStyles";
+import { Title, Line, Content, Container, Item, CopyMsg, RemoveCpy } from "./FooterStyles";
+
+async function copyNum() {
+  navigator.clipboard.writeText("+15419445116");
+  const msg = document.getElementById("copyMsg");
+  msg.style.display = "flex";
+  setTimeout(removeCopy, 7500);
+}
+
+function removeCopy() {
+  const msg = document.getElementById("copyMsg");
+  msg.style.display = "none";
+}
 
 const Footer = () => {
   return(
-    <Container>
-      <div style={{ width: "750px" }}>
-        <Title>
-          Contact Me!
-        </Title>
-        <Line />
-        <Content>
-          <Item><AiOutlinePhone />&ensp;(541) 944 - 5116</Item>
-          <Item><AiOutlineMail />&ensp;nforester351@gmail.com</Item>
-        </Content>
-      </div>
-    </Container>
+    <>
+      <Container>
+        <div style={{ width: "750px" }}>
+          <Title>
+            Contact Me!
+          </Title>
+          <Line />
+          <Content>
+            <Item onClick={copyNum} ><AiOutlinePhone />&ensp;(541) 944 - 5116</Item>
+            <Item><AiOutlineMail />&ensp;nforester351@gmail.com</Item>
+          </Content>
+        </div>
+      </Container>
+      <CopyMsg id="copyMsg">
+        Copied to Clipboard!&ensp;<RemoveCpy><AiFillCloseCircle onClick={removeCopy}/></RemoveCpy>
+      </CopyMsg>
+    </>
   )
 }
 
--- src/components/footer/FooterStyles.js
+++ src/components/footer/FooterStyles.js
@@ -38,3 +38,27 @@ export const Line = styled.div`
   background-color: ${COLOR};
   margin: auto;
 `;
+
+export const CopyMsg = styled.div`
+  color: ${COLOR};
+  background-color: rgba(25, 25, 25, 0.6);
+  font-size: 20px;
+  position: fixed;
+  top: 90vh;
+  left: 25px;
+  display: none;
+  z-index: 100;
+  padding: 10px;
+  border-left: 8px solid rgb(68, 188, 228);
+  border-radius: 5px;
+`;
+
+export const RemoveCpy = styled.div`
+  transition: 0.3s ease;
+  display: flex;
+  align-items: center;
+  &:hover {
+    filter: brightness(1.25);
+    cursor: pointer;
+  }
+`;
